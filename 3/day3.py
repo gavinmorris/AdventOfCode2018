e55c2e5b0680c60133a0fc334394ab5b99ec7b19
Gavin Morris <gavin@Gavins-MacBook-Pro.local>
2019-01-11 02:19:18 +0000

    part2 solved

--- 3/day3.py
+++ 3/day3.py
@@ -1,6 +1,7 @@
+values = {}
 with open("input.txt", "r") as fp:
     values = {}
-    overlapCount = 0
+
     for line in fp:
         #get border
         splitIt = line.split(" ")
@@ -13,7 +14,6 @@ with open("input.txt", "r") as fp:
         areas = splitIt[3].split("x")
         x = int(areas[0])
         y = int(areas[1])
-
         for i in range(0, x):
             for j in range(0, y):
                 coord = (b1+i, b2+j)
@@ -21,8 +21,30 @@ with open("input.txt", "r") as fp:
                     values[coord] += 1
                 else:
                     values[coord] = 1
-    
-        #count how many keys have values 2 or over
-    print sum(v >= 2 for v in values.values())
 
-       
+     
+    #count how many keys have values 2 or over
+    print sum(v >= 2 for v in values.values())
+with open("input.txt", "r") as fp:
+    for line in fp:
+        #get border
+        splitIt = line.split(" ")
+        id = splitIt[0]
+        borders = splitIt[2].split(",")
+        b1 = int(borders[0])
+        b2 = int(borders[1].split(":")[0])
+        
+        #get area
+        splitIt = line.split(" ")
+        areas = splitIt[3].split("x")
+        x = int(areas[0])
+        y = int(areas[1])
+        soleClaim = 0
+        for i in range(0, x):
+            for j in range(0, y):
+                coord = (b1+i, b2+j)
+                if values[coord] > 1:
+                    soleClaim += 1
+        if soleClaim == 0:
+            print id
+    
